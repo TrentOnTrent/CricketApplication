abf06f72f8cd3ede3f2c3e7096bd61e1e657261f
TrentOnTrent <Trentondunstan@hotmail.com>
2024-07-06 13:41:07 +1000

    Adjusted bash script, adding conditionals

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-python3 -m env venv
-source ../env/bin/activate
+if ! [[ -x "$(command -v python3)" ]]
+then
+  echo 'Error: 
+    This program runs on Python3, but it looks like Python3 is not installed.
+    To install Python3, check out https://www.python.org/downloads/' >&2
+  exit 1
+fi
+#python3 -m env venv
+source env/bin/activate
 # Most likely will not be needed, maybe
-#pip install -r requirements.txt
+# pip install -r requirements.txt
 
 python3 main.py
 
