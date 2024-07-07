d3e2aa658ec2736f6b707be82a7bc6a8deb3613a
TrentOnTrent <Trentondunstan@hotmail.com>
2024-07-07 13:44:34 +1000

    Continued reading JSON function, turning JSON into dictionary

--- src/fileIO.py
+++ src/fileIO.py
@@ -10,21 +10,23 @@ def load_match():
 
     """
     
-    path = './data'
+    path = 'data/'
     folder = os.fsencode(path)
     list_of_files = []
     for file in os.listdir(folder):
         filename = os.fsdecode(file)
         list_of_files.append(filename)
 
-    for index in range(0, len(list_of_files) -1):
-        print(f"{index}:  {list_of_files[index]}")
+    for index in range(0, len(list_of_files)):
+        print(f"{index + 1}:  {list_of_files[index]}")
     user_input = input("Which file would you like to open?  ")
     user_input = check_input(user_input, len(list_of_files), 1)
     selected_file = user_input - 1
     selected_file = list_of_files[selected_file]
     open_file = open(f"./data/{selected_file}")
-    print(f"{open_file}")
+    data = json.load(open_file)
+    print(f"{data[0]["Team 1"]} vs {data[0]["Team 2"]}")
+
 
 def save_match(game: dict, team1: list, team2: list):
     """
