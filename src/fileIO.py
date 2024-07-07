c7079f145e919f082d11e0183f38fdaba429c2db
TrentOnTrent <Trentondunstan@hotmail.com>
2024-07-07 13:01:21 +1000

    Added extra match, finalised json format style

--- src/fileIO.py
+++ src/fileIO.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 from datetime import date
 
 def load_match():
@@ -7,7 +9,11 @@ def load_match():
 
     """
     
-    pass
+    path = './data'
+    folder = os.fsencode(path)
+    for file in os.listdir(folder):
+        filename = os.fsdecode(file)
+        print(f"{filename}")
 
 def save_match(game: dict, team1: list, team2: list):
     """
@@ -16,11 +22,11 @@ def save_match(game: dict, team1: list, team2: list):
     """
     today = date.today()
     json_file = []
-    json_file.extend(game)
+    json_file.append(game)
     json_file.extend(team1)
     json_file.extend(team2)
     print (json.dumps(json_file))
-    filename = game["Team 1"] + " v " + game["Team 2"] + " " + str(today)
-    with open('./data/matches.json', 'w') as out_file:
+    filename = game["Team 1"]+ " v " + game["Team 2"] + " " + str(today)
+    with open(f'./data/{filename}.json', 'w+') as out_file:
         json.dump (json_file, out_file, sort_keys = True, indent = 4, ensure_ascii = False)
 
